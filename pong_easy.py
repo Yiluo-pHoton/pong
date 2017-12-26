f23b2df1d7c5c20a84e0267945a180508cd708ba
Yiluo pHoton <yiluo.li.astrophysics@gmail.com>
2017-12-26 19:17:24 +0800

    structure for pong easy

--- pong_easy.py
+++ pong_easy.py
@@ -1,2 +1,103 @@
 import Tkinter
 import gameStatus
+
+
+class Handler:
+
+    def __init__(self, game):
+        self.game = game
+
+    # More get and set functions go below here
+    # WHY USE GET AND SET FUNCTIONS:
+    # Some fields should remain unaccessible by the user
+    # therefore unaccessible outside of this class.
+    # We call this kind of field private field.
+    # In Python, we can write it as __pos_x
+    # These fields can be accessed only through get and set functions,
+    # such as get_pos_x() and set_pos_x(parameter)
+
+
+class Paddle:
+    # In the easy version of Pong game,
+    # this class is equivalent to the Player class.
+
+    def __init__(self, handler):
+        self.__handler = handler
+        # All the fields initiated by the constructor go below here
+
+    def judge_collision(self):
+        "stub"
+
+    def render(self):
+        "stub"
+
+    def update(self):
+        "stub"
+
+    def get_handler(self):
+        "stub"
+
+    # More get and set functions go below here
+    # WHY USE GET AND SET FUNCTIONS:
+    # Some fields should remain unaccessible by the user
+    # therefore unaccessible outside of this class.
+    # We call this kind of field private field.
+    # In Python, we can write it as __pos_x
+    # These fields can be accessed only through get and set functions,
+    # such as get_pos_x() and set_pos_x(parameter)
+
+
+class Ball:
+
+    def __init__(self, handler):
+        self.__handler = handler
+        # All the fields initiated by the constructor go below here
+
+    def judge_score(self):
+        "stub"
+
+    def hit(self):
+        "stub"
+
+    def render(self):
+        "stub"
+
+    def update(self):
+        "stub"
+
+    def get_handler(self):
+        "stub"
+
+    # More get and set functions go below here
+    # WHY USE GET AND SET FUNCTIONS:
+    # Some fields should remain unaccessible by the user
+    # therefore unaccessible outside of this class.
+    # We call this kind of field private field.
+    # In Python, we can write it as __pos_x
+    # These fields can be accessed only through get and set functions,
+    # such as get_pos_x() and set_pos_x(parameter)
+
+
+class Game:
+
+    def __init__(self):
+        self.__handler = handler
+        # All the fields initiated by the constructor go below here
+
+    def render(self):
+        "stub"
+
+    def update(self):
+        "stub"
+
+    def run(self):
+        "stub"
+
+    # More get and set functions go below here
+    # WHY USE GET AND SET FUNCTIONS:
+    # Some fields should remain unaccessible by the user
+    # therefore unaccessible outside of this class.
+    # We call this kind of field private field.
+    # In Python, we can write it as __pos_x
+    # These fields can be accessed only through get and set functions,
+    # such as get_pos_x() and set_pos_x(parameter)
