fb5f5c6b94e0a08f99a239530a925409e9d1e4a5
Yiluo pHoton <yiluo.li.astrophysics@gmail.com>
2017-12-30 16:18:21 +0800

    update structure

--- pong_easy.py
+++ pong_easy.py
@@ -16,6 +16,14 @@ class Handler:
     # These fields can be accessed only through get and set functions,
     # such as get_pos_x() and set_pos_x(parameter)
 
+    def get_game(self):
+        "stub"
+
+    def get_paddle(self, index):
+        "stub"
+
+    def get_ball(self):
+        "stub"
 
 class Paddle:
     # In the easy version of Pong game,
@@ -43,9 +51,6 @@ class Paddle:
     def update(self):
         "stub"
 
-    def get_handler(self):
-        "stub"
-
     # More get and set functions go below here
     # WHY USE GET AND SET FUNCTIONS:
     # Some fields should remain unaccessible by the user
@@ -54,6 +59,18 @@ class Paddle:
     # In Python, we can write it as __pos_x
     # These fields can be accessed only through get and set functions,
     # such as get_pos_x() and set_pos_x(parameter)
+
+    def get_canvas_width(self):
+        "stub"
+
+    def get_canvas_height(self):
+        "stub"
+
+    def get_padding(self):
+        "stub"
+
+    def get_pos_y(self):
+        "stub"
 
 
 class Ball:
@@ -80,9 +97,6 @@ class Ball:
     def get_canvas_height(self):
         "stub"
 
-    def get_handler(self):
-        "stub"
-
     # More get and set functions go below here
     # WHY USE GET AND SET FUNCTIONS:
     # Some fields should remain unaccessible by the user
@@ -92,6 +106,15 @@ class Ball:
     # These fields can be accessed only through get and set functions,
     # such as get_pos_x() and set_pos_x(parameter)
 
+    def get_width(self):
+        return self.__width
+
+    def get_velocity_x(self):
+        return self.__velocity_x
+
+    def get_velocity_y(self):
+        return self.__velocity_y
+
 
 class Game:
 
@@ -100,6 +123,15 @@ class Game:
         # All the fields initiated by the constructor go below here
 
     def start_new_game(self):
+        "stub"
+
+    def key_pressed_handler(self, event):
+        "stub"
+
+    def key_released_handler(self, event):
+        "stub"
+
+    def key_action(self):
         "stub"
 
     def render(self):
@@ -119,3 +151,20 @@ class Game:
     # In Python, we can write it as __pos_x
     # These fields can be accessed only through get and set functions,
     # such as get_pos_x() and set_pos_x(parameter)
+
+    def set_game_status(self, game_status):
+        "stub"
+
+    def get_game_status(self):
+        "stub"
+
+    def get_canvas_width(self):
+        "stub"
+
+    def get_canvas_height(self):
+        "stub"
+
+if __name__ == "__main__":
+    my_game = Game()
+    my_game.run()
+    my_game.frame.mainloop()
